72318cf521fbd836b4d8a02c34210620eff080c3
Ashkan Abbasi <airashkan@yahoo.com>
2020-05-29 10:06:48 -0600

    create migrations for associations and create tests

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Dish show page' do
+  describe "As a visitor" do
+    it "Can see a list of ingredients and the chef's name" do
+      
+
+  end
+
+
+
+end
+
+# Story 1 of 3
+# As a visitor
+# When I visit a dish's show page
+# I see a list of ingredients for that dish
+# and the chef's name
