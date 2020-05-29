9e0fd00df0fe32a6596bbb6940a022c4503f2fe6
Ashkan Abbasi <airashkan@yahoo.com>
2020-05-29 11:03:52 -0600

    Create dishes controller, route, and show view files.

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -3,8 +3,26 @@ require 'rails_helper'
 RSpec.describe 'Dish show page' do
   describe "As a visitor" do
     it "Can see a list of ingredients and the chef's name" do
-      
+      bob = Chef.create(name: "Bob")
+      dish1 = Dish.create(name: "Mashed Potatoes", description: "Mashed Potatoes with butter", chef_id: bob.id)
+      dish2 = Dish.create(name: "Green Beans", description: "Green Beans with butter", chef_id: bob.id)
+      potatoes = Ingredient.create(name: "potatoes", calories: 200)
+      green_beans = Ingredient.create(name: "green beans", calories: 100)
+      butter = Ingredient.create(name: "butter", calories: 300)
 
+      DishIngredient.create(dish: dish1, ingredient: potatoes)
+      DishIngredient.create(dish: dish1, ingredient: butter)
+      DishIngredient.create(dish: dish2, ingredient: green_beans)
+      DishIngredient.create(dish: dish1, ingredient: butter)
+
+      visit "dishes/#{dish1.id}"
+
+      expect(page).to have_content(dish1.name)
+      expect(page).to have_content(dish1.chef.name)
+      expect(page).to have_content(potatoes.name)
+      expect(page).to have_content(butter.name)
+
+    end
   end
 
 
